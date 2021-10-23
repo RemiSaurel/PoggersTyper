e6ab280fc6f340ad0c1c2ac1c5fd06de975367df
RémiSaurel <r.saurel5@gmail.com>
2021-10-23 11:37:42 +0200

    add: words length settings

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 let words = ["comme", "je", "son", "que", "était", "pour", "sur", "sont", "avec", "ils", "être", "un", "avoir", "ce", "par", "chaud", "mot", "mais", "que", "certains", "est", "il", "vous", "ou", "eu", "la", "de", "à", "et", "un", "dans", "nous", "boîte", "dehors", "autre", "étaient", "qui", "faire", "leur", "temps", "si", "volonté", "comment", "dit", "un", "chaque", "dire", "ne", "ensemble", "trois", "vouloir", "air", "bien", "aussi", "jouer", "petit", "fin", "mettre", "maison", "lire", "main", "port", "grand", "épeler", "ajouter", "même", "terre", "ici", "grand", "haut", "tel", "suivre", "acte", "pourquoi", "interroger", "hommes", "changement", "lumière", "genre", "de", "besoin", "maison", "image", "essayer", "nous", "encore", "animal", "point", "mère", "monde", "près", "construire", "soi", "terre", "père", "tout", "nouveau", "travail", "partie", "prendre", "obtenir", "lieu", "fabriqué", "vivre", "où", "après", "arrière", "peu", "seulement", "tour", "homme", "année", "venu", "montrer", "tous", "bon", "moi", "donner", "notre", "sous", "nom", "très", "par", "juste", "forme", "phrase", "grand", "penser", "dire", "aider", "faible", "ligne", "différer", "tour", "cause", "beaucoup", "signifier", "avant", "déménagement", "droit", "garçon", "vieux", "trop", "même", "elle", "tous", "là", "quand", "jusqu’à", "utiliser", "votre", "manière", "sur", "beaucoup", "puis", "les", "écrire", "voudrais", "comme", "si", "ces", "son", "long", "faire", "chose", "voir", "lui", "deux", "a", "regarder", "plus", "jour", "pourrait", "aller", "venir", "fait", "nombre", "son", "aucun", "plus", "personnes", "ma", "sur", "savoir", "eau", "que", "appel", "première", "qui", "peut", "vers", "côté", "été", "maintenant", "trouver", "tête", "supporter", "propre", "page", "devrait", "pays", "trouvé", "réponse", "école", "croître", "étude", "encore", "apprendre", "usine", "couvercle", "nourriture", "soleil", "quatre", "entre", "état", "garder", "il", "jamais", "dernier", "laisser", "pensée", "ville", "arbre", "traverser", "ferme", "dur", "début", "puissance", "histoire", "scie", "loin", "mer", "tirer", "gauche", "tard", "courir", "tandis", "presse", "proche", "nuit", "réel", "vie", "peu", "nord", "livre", "porter", "science", "manger", "chambre", "ami", "idée", "poisson", "montagne", "fois", "base", "entendre", "cheval", "coupe", "sûr", "regarder", "couleur", "face", "bois", "principal", "ouvert", "paraître", "ensemble", "suivant", "blanc", "enfants", "commencer", "eu", "marcher", "exemple", "facilité", "papier", "groupe", "toujours", "musique", "ceux", "marque", "souvent", "lettre", "mile", "rivière", "voiture", "pieds", "soins", "deuxième", "assez", "plaine", "fille", "habituel", "jeune", "prêt", "au-dessus", "jamais", "rouge", "liste", "bien", "sentir", "parler", "oiseau", "bientôt", "corps", "chien", "famille", "direct", "pose", "laisser", "chanson", "mesurer", "porte", "produit", "noir", "court", "chiffre", "classe", "vent", "question", "arriver", "complète", "navire", "zone", "moitié", "rock", "ordre", "feu", "sud", "problème", "pièce", "dit", "savait", "passer", "depuis", "haut", "ensemble", "roi", "rue", "pouce", "multiplier", "rien", "cours", "rester", "roue", "plein", "force", "bleu", "objet", "décider", "surface", "profond", "lune", "île", "pied", "système", "occupé", "test", "record", "bateau", "commun", "or", "possible", "plan", "place", "sec", "se", "rire", "mille", "il", "ran", "vérifier", "jeu", "forme", "assimiler", "chaud", "manquer", "apporté", "chaleur", "neige", "pneu", "apporter", "oui", "lointain", "remplir", "est", "peindre", "langue", "entre", "unité", "puissance", "ville", "fin", "certain", "voler", "tomber", "conduire", "cri", "sombre", "machine", "note", "patienter", "plan", "figure", "étoile", "boîte", "nom", "domaine", "reste", "correct", "capable", "livre", "terminé", "beauté", "entraînement", "résisté", "contenir", "avant", "enseigner", "semaine", "finale", "donné", "vert", "oh", "rapide", "développer", "océan", "chaud", "gratuit", "minute", "fort", "spécial", "esprit", "derrière", "clair", "queue", "produire", "fait", "espace", "entendu", "meilleur", "heure", "mieux", "vrai", "pendant", "cent", "cinq", "rappeler", "étape", "tôt", "tenir", "ouest", "sol", "intérêt", "atteindre", "rapide", "verbe", "chanter", "écouter", "six", "table", "voyage", "moins", "matin", "dix", "simple", "plusieurs", "voyelle", "vers", "guerre", "poser", "contre", "modèle", "lent", "centre", "amour", "personne", "argent", "servir", "apparaître", "route", "carte", "pluie", "règle", "gouverner", "tirer", "froid", "avis", "voix", "énergie", "chasse", "probable", "lit", "frère", "tour", "cellule", "croire", "peut-être", "choisir", "soudain", "compter", "carré", "raison", "longueur", "représenter", "art", "sujet", "région", "taille", "varier", "régler", "parler", "poids", "général", "glace", "question", "cercle", "paire", "inclure", "fracture", "syllabe", "feutre", "grandiose", "balle", "encore", "vague", "tomber", "présent", "lourd", "danse", "moteur", "position", "bras", "large", "voile", "matériel", "fraction", "forêt", "course", "fenêtre", "magasin", "été", "train", "sommeil", "prouver", "seul", "jambe", "exercice", "mur", "capture", "monture", "souhaiter", "ciel", "conseil", "joie", "hiver", "sat", "écrit", "sauvage", "instrument", "conservé", "verre", "herbe", "vache", "emploi", "bord", "signe", "visite", "passé", "doux", "amusement", "clair", "gaz", "temps", "mois", "million", "porter", "finition", "heureux", "espoir", "fleur", "vêtir", "étrange", "disparu", "commerce", "mélodie", "bureau", "recevoir", "rangée", "bouche", "exact", "symbole", "mourir", "moins", "difficulté", "cri", "sauf", "écrit", "semence", "ton", "joindre", "suggérer", "propre", "pause", "dame", "cour", "augmenter", "mauvais", "coup", "huile", "sang", "toucher", "cent", "mélanger", "équipe", "fil", "coût", "perdu", "brun", "porter", "jardin", "égal", "expédié", "choisir", "débit", "juste", "banque", "recueillir", "sauver", "contrôle", "décimal", "oreille", "autre", "cassé", "cas", "milieu", "tuer", "fils", "lac", "moment", "échelle", "fort", "printemps", "observer", "enfant", "droit", "consonne", "nation", "dictionnaire", "lait", "vitesse", "méthode", "organe", "payer", "âge", "section", "robe", "nuage", "surprise", "calme", "pierre", "minuscule", "montée", "frais", "conception", "pauvres", "lot", "expérience", "bas", "clé", "fer", "unique", "bâton", "plat", "vingt", "peau", "sourire", "pli", "trou", "sauter", "bébé", "huit", "village", "racine", "acheter", "augmenter", "résoudre", "métal", "si", "pousser", "sept", "paragraphe", "troisième", "doit", "cheveux", "décrire", "cuisinier", "étage", "chaque", "résultat", "brûler", "colline", "coffre-fort", "chat", "siècle", "envisager", "type", "droit", "peu", "côte", "copie", "phrase", "silencieux", "haut", "sable", "sol", "rouleau", "température", "doigt", "industrie", "valeur", "lutte", "mensonge", "battre", "exciter", "naturel", "vue", "sens", "capital", "chaise", "danger", "fruit", "riche", "épais", "soldat", "processus", "fonctionner", "pratique", "séparé", "difficile", "médecin", "protéger", "midi", "récolte", "moderne", "élément", "frapper", "étudiant", "coin", "partie", "alimentation", "dont", "localiser", "anneau", "caractère", "insecte", "pris", "période", "indiquer", "radio", "rayon", "atome", "humain", "histoire", "effet", "électrique", "attendre", "os", "rail", "imaginer", "fournir", "ainsi", "doux", "femme", "capitaine", "deviner", "nécessaire", "net", "aile", "créer", "voisin", "lavage", "chauve-souris", "plutôt", "foule", "blé", "comparer", "poème", "chaîne", "cloche", "dépendre", "viande", "rub", "tube", "célèbre", "dollar", "courant", "peur", "vue", "mince", "triangle", "planète", "chef", "colonie", "horloge", "mine", "lien", "entrer", "majeur", "frais", "recherche", "envoyer", "jaune", "pistolet", "permettre", "impression", "mort", "place", "désert", "costume", "courant", "ascenseur", "rose", "arriver", "maître", "piste", "mère", "rivage", "division", "feuille", "substance", "favoriser", "relier", "poste", "passer", "corde", "graisse", "heureux", "original", "part", "station", "papa", "pain", "charger", "propre", "bar", "proposition", "segment", "esclave", "canard", "instant", "marché", "degré", "peupler", "poussin", "cher", "ennemi", "répondre", "boisson", "support", "discours", "nature", "gamme", "vapeur", "mouvement", "chemin", "liquide", "enregistrer", "signifiait", "quotient", "dents", "coquille", "cou", "oxygène", "sucre", "décès", "assez", "compétence", "femmes", "saison", "solution", "aimant", "argent", "merci", "branche", "rencontre", "suffixe", "particulièrement", "figue", "peur", "énorme", "acier", "discuter", "avant", "similaire", "guider", "expérience", "score", "pomme", "acheté", "LED", "pas", "manteau", "masse", "carte", "bande", "corde", "glissement", "gagner", "rêver", "soirée", "condition", "alimentation", "outil", "total", "odeur", "vallée", "ni", "double", "siège", "continuer", "bloc", "graphique", "chapeau", "vendre", "succès", "entreprise", "soustraire", "événement", "particulier", "accord", "baignade", "terme", "opposé", "femme", "chaussure", "épaule", "propagation", "organiser", "camp", "inventer", "coton", "né", "déterminer", "litre", "neuf", "camion", "bruit", "niveau", "chance", "recueillir", "boutique", "tronçon", "jeter", "éclat", "propriété", "colonne", "molécule", "sélectionner", "mal", "gris", "répétition", "exiger", "large", "préparer", "sel", "nez", "pluriel", "colère", "revendication", "continent"]
 let index = 0;
+const WORD_MAX = 20, WORD_MIN = 1;
+const DEFAULT_WORD_MAX_LENGTH = 8;
+let maxLength = DEFAULT_WORD_MAX_LENGTH; // DEFAULT VALUE
+const DEFAULT_WORD_MIN_LENGTH = 3;
+let minLength = DEFAULT_WORD_MIN_LENGTH; // DEFAULT VALUE
 let selectedWords = [];
 let score = 0;
 let aps = 0;
+let accents = false;
 let solutions = new Map();
 
 window.addEventListener('load', function () {
+    // DEFAULT WORD LENGTH
+    resetLength()
     generateArray()
     validateWord()
     displayRandomWords()
@@ -14,9 +22,43 @@ window.addEventListener('load', function () {
     timer()
 });
 
+function resetAll() {
+    selectedWords = []
+    generateArray()
+    const elem = document.querySelectorAll('#displayWords span')
+    elem.forEach(e => e.remove());
+    displayRandomWords()
+    setFirstWordStyle()
+}
+
+function generateWithSettings() {
+    getSettings()
+    resetAll()
+}
+
+function getSettings() {
+    maxLength = document.getElementById('inputMaxLength').value;
+    minLength = document.getElementById('inputMinLength').value;
+}
+
+function updateMin() {
+    minLength = document.getElementById('inputMinLength').value;
+    document.getElementById('inputMaxLength').setAttribute("min", minLength)
+}
+
+function updateMax() {
+    maxLength = document.getElementById('inputMaxLength').value;
+    document.getElementById('inputMinLength').setAttribute("max", maxLength)
+}
+
 function generateArray() {
     for (let i = 0; i < 300; i++) {
-        selectedWords[i] = words[Math.floor(Math.random() * words.length)];
+        let wordToAdd = words[Math.floor(Math.random() * words.length)];
+        if (wordToAdd.length <= maxLength && wordToAdd.length >= minLength) {
+            selectedWords[i] = wordToAdd;
+        } else {
+            i--;
+        }
     }
 }
 
@@ -32,13 +74,15 @@ function displayRandomWords() {
 }
 function validateWord() {
     document.getElementById('wordInput').onkeydown = function (event) {
-        if (event.keyCode === 32 || event.keyCode === 13) {
-            document.getElementById('wordInput').innerHTML = document.getElementById('wordInput').value
+        document.getElementById('generateWithSettings').disabled = true;
+        if ((event.keyCode === 32 || event.keyCode === 13) && document.getElementById('wordInput').value.trim() !== "") {
+            document.getElementById('wordInput').innerHTML = document.getElementById('wordInput').value.trim()
             aps += document.getElementById('wordInput').value.length + 1;
             trackWord(index)
             updateScore()
             getWordInput()
             index++;
+            console.log(accents)
             return false;
         }
     }
@@ -126,7 +170,6 @@ function showScores() {
 }
 
 function showWordsErrors() {
-    console.log(solutions)
     for (let [key, value] of solutions.entries()) {
         document.getElementById('wrongWords').innerHTML += "<span class='mistakeWord'>" + value + "</span>" + " ➡ <span class='correctWord'>" + key + "</span><br>"
     }
@@ -137,7 +180,25 @@ function setFirstWordStyle() {
     document.getElementById('displayWords').getElementsByTagName('span').item(index).style.fontWeight = "bolder";
 }
 
+function resetLength() {
+    document.getElementById('inputMaxLength').value = DEFAULT_WORD_MAX_LENGTH;
+    maxLength = DEFAULT_WORD_MAX_LENGTH;
+    document.getElementById('inputMinLength').value = DEFAULT_WORD_MIN_LENGTH;
+    minLength = DEFAULT_WORD_MIN_LENGTH;
+
+    document.getElementById('inputMaxLength').setAttribute("min", minLength)
+    document.getElementById('inputMaxLength').setAttribute("max", WORD_MAX)
+    document.getElementById('inputMinLength').setAttribute("max", maxLength)
+    document.getElementById('inputMinLength').setAttribute("min", WORD_MIN)
+
+    // ADD TO HTML
+    this.document.getElementById('outputMaxLength').innerHTML = DEFAULT_WORD_MAX_LENGTH
+    this.document.getElementById('outputMinLength').innerHTML = DEFAULT_WORD_MIN_LENGTH
+}
+
 // CHECK RESET
 document.getElementById('reset').onclick = function () {
-    location.reload();
+    // RESET DEFAULT LENGTH
+    resetLength()
+    resetAll()
 }
